39fd0ebe30c913519a2dba809625bbd347757751
Aleksandr <platunov.aleksandr@mail.ru>
2023-02-26 19:08:36 +0300

    Разбор домашнего задания на семинаре

--- Seminar1/Task2.py
+++ Seminar1/Task2.py
@@ -7,6 +7,31 @@ a = int(input('Введите трехзначное, целое число: '))
 sum = 0
 while a > 0:
     sum += a % 10
-    a = a / 10
+    a = a // 10
 print(sum)
+
+
+# Решение с семинара
+
+# number = int(input('Введите трехзначное, целое число: '))
+# if 99 < number < 1000:
+#     arrayNumber = map(int, str(number))
+#     # sumDigits = arrayNumber[0] + arrayNumber[1] + arrayNumber[2]
+#     sumDigits = sum(arrayNumber) # функцией "sum" заменили то что написано выше
+#     print(sumDigits)
+# else:
+#     print('введен некоректный номер')
+
+# Решение с семинара (сложное)
+
+# my_num = input('Введите число: ')
+
+# def digit_sum(num2chek): # тут вводим любые цифры, буквы, тип "str"
+#     sum_num = 0
+#     for i in range(len(num2chek)):
+#         if num2chek[i].isdigit(): # функция ".isdigit" пробегает по всей строке, находит в ней цифры и записывает их переменную
+#             temp = int(num2chek[i]) 
+#             sum_num = sum_num + temp
+#     return sum_num
+# print('Сумма цыфр в числе', my_num, 'равна', DC(my_num)) # а тут какаято непонятка, чувак слишком умно все делает, так и не смог добить
 
--- Seminar1/Task8.py
+++ Seminar1/Task8.py
@@ -5,8 +5,19 @@
 # 3 2 4 -> yes
 # 3 2 1 -> no
 
-n, m, k = list(map(int, input('Введите необходимые параметры через пробел: ').split()))
-if k < n * m and k % n == 0 or k % m == 0:
+# n, m, k = list(map(int, input('Введите необходимые параметры через пробел: ').split()))
+# if k < n * m and k % n == 0 or k % m == 0:
+#     print('Yes')
+# else:
+#     print('No')
+
+# Решение с семинара
+
+n = int(input('Задайте длинну шоколадки в дольках: '))
+m = int(input('Задайте ширину шоколадки в дольках: '))
+k = int(input('Задайте количество долек для отлома: '))
+
+if (k % m == 0 or k % n == 0) and 0 < k < n * m:
     print('Yes')
 else:
     print('No')
