b4ec86031a556fbdbab280d84c43c379fcf010f2
Aleksandr <platunov.aleksandr@mail.ru>
2023-03-30 20:30:24 +0300

    Разбор 36 задачи на семинаре

--- Seminar7/Task36.py
+++ Seminar7/Task36.py
@@ -15,13 +15,31 @@
 # 5 10 15 20 25 30
 # 6 12 18 24 30 36
 
-rows = int(input("Введите количество строк: "))
-columns = int(input("Введите количество столбцов: "))
+# rows = int(input("Введите количество строк: "))
+# columns = int(input("Введите количество столбцов: "))
 
-def print_operation_table(operation, num_rows, num_columns):
-    a = [[operation(i, j) for j in range(1, num_columns + 1)] for i in range(1, num_rows + 1)]
-    for i in a:
-        print(*[f"{x:>3}" for x in i])
+# def print_operation_table(operation, num_rows, num_columns):
+#     a = [[operation(i, j) for j in range(1, num_columns + 1)] for i in range(1, num_rows + 1)]
+#     for i in a:
+#         print(*[f"{x:>3}" for x in i])
 
-print_operation_table(lambda x, y: x * y, rows, columns)
+# print_operation_table(lambda x, y: x * y, rows, columns)
 
+
+# ------------------------------------------------------------------------------------------------
+# решение от преподавателя
+
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    for row in range(1, num_rows + 1):
+        print(*map(operation, [row] * num_columns, range(1, num_columns + 1)), sep='\t')
+
+
+print_operation_table(lambda x, y: x * y)
+
+# попробуем описать решение начиная с "for": для каждой строки в таблице от 1 до "num_rows + 1(нужно чтобы ноль не передавался)"
+# потом сразу принт, внутри "map", принимающий "operation", кторая в свою очередь принимает два аргумента, а значит для правильной работы
+# "map" нам нужны два итерируемых объекта: 1. "[row] * num_columns" - пустой список размерностью с количеством столбиков;
+# 2. "range(1, num_columns + 1)"- это наши колонки 
+# "sep='\t'" - нужен для выравнивания размерностью в "\t" - табуляция
+
+# функция "print_operation_table" для "lambda" которая принимает два аргумента
